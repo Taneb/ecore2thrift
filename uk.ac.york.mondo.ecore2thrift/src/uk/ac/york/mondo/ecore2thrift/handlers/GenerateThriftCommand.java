6f9aab73f8fd50cbd659b6298f2cbf9e58d4c536
Nathan van Doorn <nvd1234@gmail.com>
2015-08-06 10:17:13 +0100

    Run validation before transforming

--- uk.ac.york.mondo.ecore2thrift/src/uk/ac/york/mondo/ecore2thrift/handlers/GenerateThriftCommand.java
+++ uk.ac.york.mondo.ecore2thrift/src/uk/ac/york/mondo/ecore2thrift/handlers/GenerateThriftCommand.java
@@ -3,14 +3,18 @@ package uk.ac.york.mondo.ecore2thrift.handlers;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.ecore.EValidator;
 import org.eclipse.epsilon.egl.EglFileGeneratingTemplate;
 import org.eclipse.epsilon.egl.EglFileGeneratingTemplateFactory;
 import org.eclipse.epsilon.egl.EglTemplateFactoryModuleAdapter;
@@ -19,6 +23,8 @@ import org.eclipse.epsilon.egl.status.StatusMessage;
 import org.eclipse.epsilon.emc.emf.EmfModel;
 import org.eclipse.epsilon.eol.IEolExecutableModule;
 import org.eclipse.epsilon.eol.exceptions.models.EolModelLoadingException;
+import org.eclipse.epsilon.evl.EvlModule;
+import org.eclipse.epsilon.evl.EvlUnsatisfiedConstraint;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -28,8 +34,7 @@ import uk.ac.york.mondo.ecore2thrift.Activator;
 public class GenerateThriftCommand extends AbstractHandler implements IHandler {
 	private static final String ECORE_URI = "http://www.eclipse.org/emf/2002/Ecore";
 	
-	private void addModelToFactoryFromFile(EglFileGeneratingTemplateFactory factory, File file) throws EolModelLoadingException{
-		final IEolExecutableModule eglModule = new EglTemplateFactoryModuleAdapter(factory);
+	private void addModelFromFile(IEolExecutableModule eglModule, File file) throws EolModelLoadingException{
 		final EmfModel model = new EmfModel();
 		model.setModelFile(file.getAbsolutePath());
 		model.setName(file.getName());
@@ -46,15 +51,47 @@ public class GenerateThriftCommand extends AbstractHandler implements IHandler {
 	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
 		final ISelection selection0 = HandlerUtil.getCurrentSelectionChecked(event);
 		if (selection0 instanceof IStructuredSelection) {
 			final IStructuredSelection selection = (IStructuredSelection)selection0; 
 			final IFile ecore = (IFile)selection.getFirstElement();
 			final File ecoreFile = ecore.getLocation().toFile();
+			// run EVL script
+			try {
+				for (IMarker marker : ecore.findMarkers(EValidator.MARKER, false, IResource.DEPTH_INFINITE)) {
+					if (marker.getAttribute("secondary-marker-type", "").equalsIgnoreCase("uk.ac.york.mondo.ecore2thift.validation")) {
+						marker.delete();
+					}
+				}
+				EvlModule validateModule = new EvlModule();
+				addModelFromFile(validateModule, ecoreFile);
+				validateModule.parse(GenerateThriftCommand.class.getResource("/epsilon/ecore2thrift.evl").toURI());
+				validateModule.execute();
+				
+				List<EvlUnsatisfiedConstraint> unsatisfiedConstraints = validateModule.getContext().getUnsatisfiedConstraints();
+				if (!unsatisfiedConstraints.isEmpty()) {
+					boolean shouldStop = false;
+					for (EvlUnsatisfiedConstraint unsatisfiedConstraint : unsatisfiedConstraints) {
+						IMarker marker = ecore.createMarker(EValidator.MARKER);
+						if (unsatisfiedConstraint.getConstraint().isCritique()) {
+							marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_WARNING);
+						}
+						else {
+							marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
+							shouldStop = true;
+						}
+						marker.setAttribute(IMarker.MESSAGE, unsatisfiedConstraint.getMessage());
+						marker.setAttribute("secondary-marker-type", "uk.ac.york.mondo.ecore2thift.validation");
+					}
+					if (shouldStop) return null; //TODO: show dialogue saying that there are problems					
+				}
+			} catch (Exception e) {
+				// deal with exception
+			}
 			final EglFileGeneratingTemplateFactory factory = new EglFileGeneratingTemplateFactory();
 			try {
-				addModelToFactoryFromFile(factory, ecoreFile);
+				final IEolExecutableModule eglModule = new EglTemplateFactoryModuleAdapter(factory);
+				addModelFromFile(eglModule, ecoreFile);
 			} catch (Exception e) {
 				Activator.getPlugin().logError("There was an error while loading the model", e);
 				return null;
